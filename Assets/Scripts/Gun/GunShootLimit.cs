c87be61454dfd6fa7605c05b6da24fe2d9aadf00
ltzan87 <100611591+ltzan87@users.noreply.github.com>
2022-07-12 14:28:58 -0300

    add gun UI

--- Assets/Scripts/Gun/GunShootLimit.cs
+++ Assets/Scripts/Gun/GunShootLimit.cs
@@ -1,14 +1,22 @@
+using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GunShootLimit : GunBase
 {
+    public List<UIGunUpdater> uIGunUpdaters;
+
     public float maxShoot = 5f;
     public float timeToRecharge = 1f;
     
     private float _currentShoots;
     private bool _recharging = false;
+
+
+    private void Awake() {
+        GetAllUIs();
+    }
 
     protected override IEnumerator ShootCoroutine()
     {
@@ -21,6 +29,7 @@ public class GunShootLimit : GunBase
                 Shoot();
                 _currentShoots++;
                 CheckRecharge();
+                UpdateUI();
                 yield return new WaitForSeconds(timeBetweenShoot);
             }
         }
@@ -28,7 +37,7 @@ public class GunShootLimit : GunBase
 
     private void CheckRecharge()
     {
-        if(_currentShoots < maxShoot)
+        if(_currentShoots >= maxShoot)
         {
             StopShoot();
             StartRecharge();
@@ -47,11 +56,21 @@ public class GunShootLimit : GunBase
         while(time < timeToRecharge)
         {
             time += Time.deltaTime;
-            Debug.Log("Recharging: " + time);
+            uIGunUpdaters.ForEach(i => i.UpdateValue(time/timeToRecharge));
             yield return new WaitForEndOfFrame();
         }
 
         _currentShoots = 0;
         _recharging = false;
+    }
+
+    private void UpdateUI()
+    {
+        uIGunUpdaters.ForEach(i => i.UpdateValue(maxShoot, _currentShoots));
+    }
+
+    private void GetAllUIs()
+    {
+        uIGunUpdaters = GameObject.FindObjectsOfType<UIGunUpdater>().ToList();
     }
 }
