42e7f6119233cd63aa91176a88b1ab5108c0ac9e
ltzan87 <100611591+ltzan87@users.noreply.github.com>
2022-07-21 18:39:18 -0300

    add dropping itens of DestructableItem

--- Assets/Scripts/Itens/DestructableItem/DestructableItemBase.cs
+++ Assets/Scripts/Itens/DestructableItem/DestructableItemBase.cs
@@ -10,6 +10,10 @@ public class DestructableItemBase : MonoBehaviour
     public float shakeDuration = .1f;
     public int shakeForce = 1;
 
+    public int dropCoinsAmout = 10;
+    public GameObject coinPrefab;
+    public Transform dropPoisiton;
+
 
     private void OnValidate() {
         if(healthBase == null) healthBase = GetComponent<HealthBase>();
@@ -23,5 +27,29 @@ public class DestructableItemBase : MonoBehaviour
     private void OnDamage(HealthBase h)
     {
         transform.DOShakeScale(shakeDuration, Vector3.up/2, shakeForce);
+        DropGroupOfCoins();
+    }
+
+    [NaughtyAttributes.Button]
+    private void DropCoins()
+    {
+        var i = Instantiate(coinPrefab);
+        i.transform.position = dropPoisiton.position;
+        i.transform.DOScale(0, 1f).SetEase(Ease.OutBack).From();
+    }
+
+    [NaughtyAttributes.Button]
+    private void DropGroupOfCoins()
+    {
+        StartCoroutine(DropGroupOfCoinsCoroutine());
+    }
+
+    IEnumerator DropGroupOfCoinsCoroutine()
+    {
+        for(int i = 0; i < dropCoinsAmout; i++)
+        {
+            DropCoins();
+            yield return new WaitForSeconds(.1f);
+        }
     }
 }
